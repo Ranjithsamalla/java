33e1409d3bb58f1e74602b1deb091e8017138c8f
Ranjith Samalla <Ranjithsamalla>
2018-11-28 17:30:16 +0530

    something added sub branch

---
+++ java/src/main/java/SampleExample/BreakExample.java
@@ -0,0 +1,16 @@
+package SampleExample;
+
+public class BreakExample {
+	public static void main(String[] args) {  
+		for(int i=1;i<=10;i++){  
+			if(i==10)
+			{  
+				System.out.println("Program is breaking for if condition satisifed");
+				break;  
+			}
+
+			System.out.println(i);  
+		}  
+	}  
+
+}
